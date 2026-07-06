912feac39248b7578228b26ba07413027b82a7ef
Kayla amirul alma <2313020242@mhs.univ.ac.id>
2026-07-07 00:37:14 +0700

    [FE] Memperbaiki integrasi frontend dan metadata Docker

--- views/app.js
+++ views/app.js
@@ -11,7 +11,7 @@ async function loadData() {
         dataList.innerHTML = "";
 
         const response = await fetch(
-            "http://127.0.0A.1:5000/api/data"
+            "http://127.0.0.1:5000/api/data"
         );
 
         if (!response.ok) {
